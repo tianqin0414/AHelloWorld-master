dece394040880d6a94195aed2ecf5365dd852602
ZeroIos <387956977@qq.com>
2018-08-03 10:51:25 +0800

    0803

--- src/components/TQItem.js
+++ src/components/TQItem.js
@@ -8,41 +8,41 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
+	StyleSheet,
+	Text,
+	View,
+	Image,
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  root: {
-    marginTop: 20,
-    width: 150,
-  },
-  image: {
-    width: 150,
-    height: 215,
-  },
-  title: {
-    marginTop: 5,
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  }
+	root: {
+		marginTop: 20,
+		width: 150,
+	},
+	image: {
+		width: 150,
+		height: 215,
+	},
+	title: {
+		marginTop: 5,
+		fontSize: 20,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	}
 });
 
 export default  class MyApp extends  Component {
-  render() {
-    return (
-      <View style={styles.root}>
+	render() {
+		return (
+			<View style={styles.root}>
 
-        <Image
-          source = {require('./img/poster.jpg')}
-          style={styles.image}
-        />
-        <Text style={styles.title} numberOfLines={1}>金刚狼3:殊死一战</Text>
-      </View>
-    );
-  }
+				<Image
+					source = {require('./img/poster.jpg')}
+					style={styles.image}
+				/>
+				<Text style={styles.title} numberOfLines={1}>金刚狼3:殊死一战</Text>
+			</View>
+		);
+	}
 
 }
